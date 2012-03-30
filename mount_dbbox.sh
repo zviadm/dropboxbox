208cdee5748f5ec4325ca251e98052ad60b408c9
Zviad Metreveli <zvio2002@gmail.com>
2012-03-29 23:50:10 -0700

    add script to launch dbbox

---
+++ mount_dbbox.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+DBBOX_IMG=/tmp/dbbox_img
+DBBOX=/tmp/dbbox
+
+sudo umount -f $DBBOX || true
+sudo umount -f $DBBOX_IMG || true
+
+sudo mkdir -p $DBBOX
+sudo mkdir -p $DBBOX_IMG
+
+sudo ./dbbox $DBBOX_IMG -d &
+FUSE_PID=$!
+
+sleep 5
+sudo mount $DBBOX_IMG/dbbox.img $DBBOX -t msdos -o loop,ro,noexec || sudo kill $FUSE_PID
+wait $FUSE_PID
